9c1a81563b10532e45a2977ceb709b94535e4965
dizaynkentbilisim <dizaynkentyonetim@gmail.com>
2020-06-24 23:29:01 +0300

    Proje Katmanları

---
+++ MusicMarketETicaret.Utility/PorojectConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicMarketETicaret.Utility
+{
+  public static class PorojectConstant
+    {
+        public const string ResultNotFound = "Data Not Found";
+    }
+}
