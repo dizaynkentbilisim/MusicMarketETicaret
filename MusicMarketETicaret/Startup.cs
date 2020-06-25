b8f093a004d0abdffa4b41c01cc2a540e12d1a8e
dizaynkentbilisim <dizaynkentyonetim@gmail.com>
2020-06-25 23:15:27 +0300

    yedek

--- MusicMarketETicaret/Startup.cs
+++ MusicMarketETicaret/Startup.cs
@@ -62,7 +62,7 @@ namespace MusicMarketETicaret
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
