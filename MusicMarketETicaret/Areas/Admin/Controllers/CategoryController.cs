7fbf78dde98c79ab1f2fed4ad7406d7392080986
dizaynkentbilisim <dizaynkentyonetim@gmail.com>
2020-06-29 23:52:15 +0300

    Controller & View firs bagin

---
+++ MusicMarketETicaret/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicMarketETicaret.DataAccess.MainRepository;
+using MusicMarketETicaret.Models.DbModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicMarketETicaret.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    
+    public class CategoryController : Controller
+    {
+        #region Variables
+        private readonly UnitOfWork _uow;
+        #endregion
+
+        #region CTOR
+        public CategoryController(UnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        #endregion
+
+        #region Actions
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        #endregion
+
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            var allObj = _uow.category.GetAll();
+            return Json(new { data = allObj });
+
+        }
+        #endregion
+    }
+}
--- MusicMarketETicaret/Startup.cs
+++ MusicMarketETicaret/Startup.cs
@@ -27,13 +27,14 @@ namespace MusicMarketETicaret
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("SqlConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();//Kullanmamýþsýn:=
-            services.AddRazorPages();
+         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
